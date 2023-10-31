59518064f09461f98be39236848ae064fae20367
Cody Meuer <93593334+stiltlunar@users.noreply.github.com>
2023-10-31 09:58:38 -0500

    environment variables

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const pgp = require('pg-promise')();
+require('dotenv').config();
 
 const cn = {
-  host: 'localhost',
-  port: 5432,
-  database: 'bibledb',
-  user: 'codymeuer',
-  password: '',
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DB,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASS,
   max: 30 // use up to 30 connections
 
   // "types" - in case you want to set custom type parsers on the pool level
@@ -14,7 +15,7 @@ const cn = {
 const db = pgp(cn);
 
 async function query() {
-  const data = await db.any('SELECT * FROM bible_versions')
+  const data = await db.any('SELECT * FROM bible_versions').catch((error) => {console.log('ERROR: ' + error)})
   console.log(data)
 }
 
